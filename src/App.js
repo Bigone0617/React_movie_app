ed0085673e0a821c2b72e1c2a4f4e2804cda6875
Bigone0617 <xodlfsha@gmail.com>
2021-03-11 14:35:12 +0900

    Revert "setting&component"
    This reverts commit 26731082ef49da0336e8fd4b03c27420601b0ab9.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-function Food({fav}) {
-  return <h1>I like {fav}!</h1>
-}
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>aaa</h1>
-      <Food fav="kimchi"/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
