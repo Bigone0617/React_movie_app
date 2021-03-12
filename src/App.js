af4d5b65d93b554e2e541e18fa20396f44905dba
Bigone0617 <xodlfsha@gmail.com>
2021-03-12 19:26:38 +0900

    proptype

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-function Food({fav}) {
-  return <h1>I like {fav}!</h1>
+import PropTypes from "prop-types";
+
+const itemList = [
+  {
+    id : 1,
+    name : "kimchi"
+  },
+  {
+    id : 2,
+    name : "ramen"
+  },
+  {
+    id : 3,
+    name : "bulgogi"
+  },
+  {
+    id : 4,
+    name : "pizza"
+  },
+  {
+    id : 5,
+    name : "coffee"
+  }
+];
+
+function Food({name}) {
+  return <h1>I like {name}!</h1>
+};
+
+// 필수값 체크, 형식체크
+Food.propTypes = {
+  name: PropTypes.string.isRequired
 }
 
 function App() {
   return (
     <div className="App">
-      <h1>aaa123</h1>
-      <Food fav="kimchi"/>
+      {
+        itemList.map(item => {
+          return <Food key={item.id} name={item.name}/>
+        })
+      }
     </div>
   );
-}
+};
 
 export default App;
